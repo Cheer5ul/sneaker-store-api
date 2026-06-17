8452fe1fcbbeec79b7c2d3ecf40b03cc63b6d1fd
Cheerful <yura08az@gmail.com>
2026-06-17 12:04:16 +0300

    feat: create SneakerSizeErrors

---
+++ SneakerStore.Core/Results/Errors/Sneaker/SneakerSizeErrors.cs
@@ -0,0 +1,16 @@
+﻿namespace SneakerStore.Core.Results.Errors.Sneaker;
+
+public static class SneakerSizeErrors
+{
+    private static class Codes
+    {
+        public const string InvalidSize = "SneakerSize.InvalidSize"; 
+        public const string InvalidRemainedInStockNumber = "SneakerSize.InvalidRemainedInStockNumber";
+    }
+    
+    public static Error InvalidSize(decimal size)
+        => new Error(Codes.InvalidSize, $"Size {size} is invalid.");
+    
+    public static Error InvalidRemainedInStockNumber(int remainedInStock)
+        => new Error(Codes.InvalidRemainedInStockNumber, $"Amount of remained items {remainedInStock} is invalid.");
+}
