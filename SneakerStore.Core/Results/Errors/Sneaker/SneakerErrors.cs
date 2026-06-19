527644ecf2a153f30f5670f9e999b5b9fab3fd43
Cheerful <yura08az@gmail.com>
2026-06-19 21:09:21 +0300

    feat: add InvalidImageUrl error to SneakerErrors.cs

--- SneakerStore.Core/Results/Errors/Sneaker/SneakerErrors.cs
+++ SneakerStore.Core/Results/Errors/Sneaker/SneakerErrors.cs
@@ -9,6 +9,7 @@ public static class SneakerErrors
         public const string InvalidDescription = "Sneaker.InvalidDescription";
         public const string InvalidPrice = "Sneaker.InvalidPrice";
         public const string NotFound = "Sneaker.NotFound";
+        public const string InvalidImageUrl = "Sneaker.InvalidImageUrl";
     }
 
     public static Error InvalidName(string name)
@@ -21,4 +22,7 @@ public static class SneakerErrors
         => new Error(Codes.InvalidPrice, $"Price {price} is invalid.");
     public static Error NotFound(Guid id)
         => new Error(Codes.NotFound, $"Sneaker with id {id} is not found.");
+    
+    public static Error InvalidImageUrl(string imageUrl)
+        => new Error(Codes.InvalidImageUrl, $"ImageUrl {imageUrl} is invalid.");
 }
