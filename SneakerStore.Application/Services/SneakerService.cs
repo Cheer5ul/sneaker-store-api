bab2b8c266b2c87f24de09f59a74d1603f914412
Cheerful <yura08az@gmail.com>
2026-06-25 22:13:34 +0300

    feat: add Create & Delete methods to SneakerService.cs

--- SneakerStore.Application/Services/SneakerService.cs
+++ SneakerStore.Application/Services/SneakerService.cs
@@ -1,17 +1,58 @@
-﻿using SneakerStore.Core.Interfaces.Repositories.Sneaker;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading;
+using System.Threading.Tasks;
+using SneakerStore.Application.DTOs.Sneaker;
+using SneakerStore.Core.Interfaces.Repositories.Sneaker;
 using SneakerStore.Core.Models.Sneaker;
+using SneakerStore.Core.Results;
+using SneakerStore.Core.Results.Errors.Sneaker;
 
 namespace SneakerStore.Application.Services;
 
 public class SneakerService(ISneakerRepository sneakerRepository)
 {
-    public async Task<List<Sneaker>> GetAll(CancellationToken cancellationToken = default)
+    public async Task<Result<List<Sneaker>>> GetAll(CancellationToken cancellationToken = default)
     {
-        return await sneakerRepository.GetAll(cancellationToken);
+        var sneakersListResult = await sneakerRepository.GetAll(cancellationToken);
+        return Result<List<Sneaker>>.Success(sneakersListResult);
     }
 
-    // public async Task<Sneaker> Create()
-    // {
-    //     
-    // }
+    public async Task<Result<Guid>> Create(CreateSneakerDto sneakerDto,
+        CancellationToken cancellationToken = default)
+    {
+        // Passing Core validation
+        var sneakerResult = Sneaker.Create(
+            sneakerDto.Name,
+            sneakerDto.Price,
+            sneakerDto.Description,
+            sneakerDto.Sizes
+                .Select(ssDto => (ssDto.Size, ssDto.RemainedInStock)),
+            sneakerDto.ImageUrl
+        );
+
+        if (sneakerResult.IsFailure)
+        {
+            return Result<Guid>.Failure(sneakerResult.Errors);
+        }
+
+        var sneakerGuid = await sneakerRepository.Create(
+            sneakerResult.Value!, cancellationToken);
+        
+        return Result<Guid>.Success(sneakerGuid);
+    }
+
+    public async Task<Result> Delete(Guid id, CancellationToken cancellationToken = default)
+    {
+        // Making sure the sneaker exists
+        var sneakerExists = await sneakerRepository.SneakerExists(
+            id, cancellationToken);
+
+        if (sneakerExists is false) return Result.Failure([SneakerErrors.NotFound(id)]);
+        
+        await sneakerRepository.Delete(id, cancellationToken);
+        return Result.Success();
+    }
 }
