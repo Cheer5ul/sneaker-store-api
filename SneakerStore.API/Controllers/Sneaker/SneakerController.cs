0ffab30d41396abd3912005eb99085ffd303aa44
Cheerful <yura08az@gmail.com>
2026-07-01 15:34:53 +0300

    feat: add UpdateName, Price & Delete methods(endpoints)

--- SneakerStore.API/Controllers/Sneaker/SneakerController.cs
+++ SneakerStore.API/Controllers/Sneaker/SneakerController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SneakerStore.Application.DTOs.Sneaker;
 using SneakerStore.Application.Services;
-using SneakerStore.Core.Interfaces.Repositories.Sneaker;
 using SneakerStore.DTOs.Sneaker;
+using SneakerStore.FailureHandler;
 
 namespace SneakerStore.Controllers.Sneaker;
 
@@ -13,18 +13,20 @@ public class SneakerController : ControllerBase
     // TODO: Add Error Handler
     
     private readonly ISneakerService _sneakerService;
-
-    public SneakerController(ISneakerService sneakerService)
+    private readonly IFailureHandler _failureHandler;
+    public SneakerController(ISneakerService sneakerService,
+        IFailureHandler failureHandler)
     {
         _sneakerService = sneakerService;
+        _failureHandler = failureHandler;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<SneakerResponseDto>>> GetAll(CancellationToken cancellationToken)
     {
         var result = await  _sneakerService.GetAll(cancellationToken);
-        
-        // validation
+
+        if (result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
 
         var sneakerList = result.Value!;
 
@@ -45,18 +47,50 @@ public class SneakerController : ControllerBase
         return Ok(sneakerResponses);
     }
     
-    // public async Task<ActionResult<Guid>> Create(CreateSneakerDto createSneakerDto,
-    //     CancellationToken cancellationToken)
-    // {
-    //     var result = await _sneakerService.Create(createSneakerDto, cancellationToken);
-    //
-    //     if (result.IsFailure)
-    //     {
-    //         // return null;
-    //     }
-    //
-    //     var response = result.Value!;
-    //     
-    //     return Ok(response);
-    // }
+    [HttpPost("create-sneaker")]
+    public async Task<ActionResult<Guid>> Create(CreateSneakerDto createSneakerDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _sneakerService.Create(createSneakerDto, cancellationToken);
+    
+        if (result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+    
+        var response = result.Value!;
+        
+        return Ok(response);
+    }
+
+    [HttpPatch("{id:guid}/name")]
+    public async Task<ActionResult> UpdateName(Guid id, [FromBody] UpdateNameDto updateNameDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _sneakerService.UpdateName(id, updateNameDto.Name, cancellationToken);
+
+        if (result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+        
+        
+        return Ok();
+    }
+    
+    [HttpPatch("{id:guid}/price")]
+    public async Task<ActionResult> UpdatePrice(Guid id, [FromBody] UpdatePriceDto updatePriceDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await _sneakerService.UpdatePrice(id, updatePriceDto.Price, cancellationToken);
+        
+        if(result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+        
+        return Ok();
+    }
+
+    public async Task<ActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _sneakerService.Delete(id, cancellationToken);
+        
+        if (result.IsFailure) return _failureHandler.HandleFailure(result, HttpContext);
+        
+        return Ok();
+    }
+    
+    
 }
