27e5b70a3808b350950a4bca3199fcb880f236a6
Cheerful <yura08az@gmail.com>
2026-07-01 15:44:04 +0300

    feat: add dependencies & problemDetails settings

--- SneakerStore.API/Program.cs
+++ SneakerStore.API/Program.cs
@@ -1,17 +1,41 @@
 using Microsoft.EntityFrameworkCore;
+using SneakerStore.Application.Services;
+using SneakerStore.Core.Interfaces.Repositories.Sneaker;
+using SneakerStore.FailureHandler;
 using SneakerStore.Persistence;
+using SneakerStore.Persistence.Repositories.Sneaker;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddProblemDetails(configure =>
+{
+    configure.CustomizeProblemDetails = context =>
+    {
+        context.ProblemDetails.Extensions.TryAdd("requestId", context.HttpContext.TraceIdentifier);
+    };
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connecting db
 builder.Services.AddDbContext<SneakerStoreDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString(nameof(SneakerStoreDbContext)));
 });
 
+// Repositories
+builder.Services.AddScoped<ISneakerRepository, SneakerRepository>();
+
 // Services
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISneakerService, SneakerService>();
+
+// Failure Handlers
+builder.Services.AddScoped<IFailureHandler, FailureHandler>();
+
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
@@ -23,5 +47,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
